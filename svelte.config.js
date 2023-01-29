c5f9ee5e804ccc50ebdf5faa40d6569c0c358b55
Rob Davis <bfe5e925@opayq.com>
2023-01-29 21:22:19 +1000

    Working on cps

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,22 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // https://rodneylab.com/sveltekit-content-security-policy/
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ precompress: true }),
         csp: {
 
-            mode: "hash",
-            directives: { "script-src": ["self"] }
+            mode: "auto",
+            directives: { "script-src": ["self"] },
      
-        }
+        },
+        appDir: 'ext',
 	}
 };
 
